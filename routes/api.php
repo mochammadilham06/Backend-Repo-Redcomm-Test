c3f9bd39a05553f140f2e5679a692224236eda64
mochammadilham06 <stormsykres1006@gmail.com>
2023-06-25 13:44:26 +0700

    add new

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/comments', [CommentController::class, 'index']);
+Route::apiResource('products', ProductController::class);
 
